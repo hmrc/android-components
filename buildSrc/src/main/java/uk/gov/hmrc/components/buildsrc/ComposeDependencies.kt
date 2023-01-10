ffba425c6cfa58d1004e1d8a072871b4144aae0b
Jemma <7998113+jemmaSlater@users.noreply.github.com>
2023-01-10 18:32:04 +0000

    Update target/compile sdks, add compose bom

--- buildSrc/src/main/java/uk/gov/hmrc/components/buildsrc/ComposeDependencies.kt
+++ buildSrc/src/main/java/uk/gov/hmrc/components/buildsrc/ComposeDependencies.kt
@@ -18,24 +18,26 @@ package uk.gov.hmrc.components.buildsrc
 class ComposeDependencies {
     object Versions {
         const val minSdk = 23
-        const val targetSdk = 31
-        const val compileSdk = 31
-        const val compose = "1.0.5"
+        const val targetSdk = 33
+        const val compileSdk = 33
         const val kotlinComposeCompiler = "1.5.21"
+        const val kotlinCompilerExtensionVersion = "1.3.2"
     }
 
     object Libs {
+        const val compose_bom = "androidx.compose:compose-bom:2022.12.00"
+
         const val material = "com.google.android.material:material:1.4.0"
 
         const val androidX_coreKtx = "androidx.core:core-ktx:1.7.0"
         const val androidX_activity_activityCompose = "androidx.activity:activity-compose:1.4.0"
         const val androidX_appCompat = "androidx.appcompat:appcompat:1.4.0"
 
-        const val androidX_compose_foundation = "androidx.compose.foundation:foundation:${Versions.compose}"
-        const val androidX_compose_material = "androidx.compose.material:material:${Versions.compose}"
-        const val androidX_compose_tooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
-        const val androidX_compose_ui = "androidx.compose.ui:ui:${Versions.compose}"
-        const val androidX_compose_uiTest = "androidx.compose.ui:ui-test-junit4:${Versions.compose}"
+        const val androidX_compose_foundation = "androidx.compose.foundation:foundation"
+        const val androidX_compose_material = "androidx.compose.material:material"
+        const val androidX_compose_tooling = "androidx.compose.ui:ui-tooling"
+        const val androidX_compose_ui = "androidx.compose.ui:ui"
+        const val androidX_compose_uiTest = "androidx.compose.ui:ui-test-junit4"
 
         const val androidX_lifecycle_runtimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.4.0"
         const val androidX_navigation_navigationCompose = "androidx.navigation:navigation-compose:2.4.0-beta02"
--- buildSrc/src/main/java/uk/gov/hmrc/components/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/uk/gov/hmrc/components/buildsrc/Dependencies.kt
@@ -18,8 +18,8 @@ package uk.gov.hmrc.components.buildsrc
 class Dependencies {
     object Versions {
         const val minSdk = 23
-        const val targetSdk = 30
-        const val compileSdk = 30
+        const val targetSdk = 33
+        const val compileSdk = 33
         const val buildTools = "30.0.3"
         const val kotlin = "1.5.31"
     }
