fe0078ceddc43751ea9e6268da98926b4b86af37
Jemma <7998113+jemmaSlater@users.noreply.github.com>
2023-01-11 18:57:06 +0000

    Update compose

--- buildSrc/src/main/java/uk/gov/hmrc/components/buildsrc/ComposeDependencies.kt
+++ buildSrc/src/main/java/uk/gov/hmrc/components/buildsrc/ComposeDependencies.kt
@@ -25,23 +25,22 @@ class ComposeDependencies {
 
     object Libs {
         const val compose_bom = "androidx.compose:compose-bom:2022.12.00"
+        const val compose_foundation = "androidx.compose.foundation:foundation"
+        const val compose_material = "androidx.compose.material:material"
+        const val compose_tooling = "androidx.compose.ui:ui-tooling"
+        const val compose_ui = "androidx.compose.ui:ui"
+        const val compose_uiTest = "androidx.compose.ui:ui-test-junit4"
 
-        const val material = "com.google.android.material:material:1.4.0"
+        const val material = "com.google.android.material:material:1.7.0"
 
-        const val androidX_coreKtx = "androidx.core:core-ktx:1.7.0"
-        const val androidX_activity_activityCompose = "androidx.activity:activity-compose:1.4.0"
-        const val androidX_appCompat = "androidx.appcompat:appcompat:1.4.0"
+        const val androidX_activity_activityCompose = "androidx.activity:activity-compose:1.6.1"
+        const val androidX_appCompat = "androidx.appcompat:appcompat:1.5.1"
+        const val androidX_coreKtx = "androidx.core:core-ktx:1.9.0"
+        const val androidX_lifecycle_runtimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1"
+        const val androidX_navigation_navigationCompose = "androidx.navigation:navigation-compose:2.5.3"
 
-        const val androidX_compose_foundation = "androidx.compose.foundation:foundation"
-        const val androidX_compose_material = "androidx.compose.material:material"
-        const val androidX_compose_tooling = "androidx.compose.ui:ui-tooling"
-        const val androidX_compose_ui = "androidx.compose.ui:ui"
-        const val androidX_compose_uiTest = "androidx.compose.ui:ui-test-junit4"
-
-        const val androidX_lifecycle_runtimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.4.0"
-        const val androidX_navigation_navigationCompose = "androidx.navigation:navigation-compose:2.4.0-beta02"
-        const val androidX_test_ext_junit = "androidx.test.ext:junit-ktx:1.1.3"
-        const val androidX_test_espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
+        const val androidX_test_ext_junit = "androidx.test.ext:junit-ktx:1.1.5"
+        const val androidX_test_espressoCore = "androidx.test.espresso:espresso-core:3.5.1"
 
         const val junit = "junit:junit:4.13.2"
     }
