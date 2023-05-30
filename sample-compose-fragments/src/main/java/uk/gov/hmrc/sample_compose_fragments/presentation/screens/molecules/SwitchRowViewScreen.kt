508cc52c5c4a24f505276a0a53c804b539106f2b
Jemma <7998113+jemmaSlater@users.noreply.github.com>
2023-05-30 19:31:26 +0100

    Set up classes

---
+++ sample-compose-fragments/src/main/java/uk/gov/hmrc/sample_compose_fragments/presentation/screens/molecules/SwitchRowViewScreen.kt
@@ -0,0 +1,19 @@
+package uk.gov.hmrc.sample_compose_fragments.presentation.screens.molecules
+
+import androidx.compose.runtime.Composable
+import uk.gov.hmrc.sample_compose_fragments.presentation.screens.sampletemplate.ExamplesSlot
+import uk.gov.hmrc.sample_compose_fragments.presentation.screens.sampletemplate.PlaceholderSlot
+import uk.gov.hmrc.sample_compose_fragments.presentation.screens.sampletemplate.ScreenScrollViewColumn
+
+@Composable
+fun SwitchRowViewScreen() {
+    ScreenScrollViewColumn {
+        PlaceholderSlot {
+
+        }
+
+        ExamplesSlot {
+
+        }
+    }
+}
