767d79f1ee26a2dff11b07ba38f0a14f28f325b8
aliahmedbd <11981999+aliahmedbd@users.noreply.github.com>
2023-06-28 11:48:26 +0100

    Safe args updated with 2.6.0-alpha

--- buildSrc/src/main/java/uk/gov/hmrc/components/buildsrc/Kotlin.kt
+++ buildSrc/src/main/java/uk/gov/hmrc/components/buildsrc/Kotlin.kt
@@ -16,6 +16,6 @@ package uk.gov.hmrc.components.buildsrc
  * limitations under the License.
  */
 object Kotlin {
-    const val version = "1.7.20"
+    const val version = "1.8.20"
     const val jvmVersion = "1.8"
 }
