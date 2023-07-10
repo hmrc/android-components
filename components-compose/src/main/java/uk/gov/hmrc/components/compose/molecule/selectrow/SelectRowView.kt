e234b635a434652c0fba4aa53e979dc190e755bb
aliahmedbd <11981999+aliahmedbd@users.noreply.github.com>
2023-07-10 17:57:43 +0100

    Select Row View functionality added

--- components-compose/src/main/java/uk/gov/hmrc/components/compose/molecule/selectrow/SelectRowView.kt
+++ components-compose/src/main/java/uk/gov/hmrc/components/compose/molecule/selectrow/SelectRowView.kt
@@ -15,12 +15,110 @@
  */
 package uk.gov.hmrc.components.compose.molecule.selectrow
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import uk.gov.hmrc.components.compose.R
+import uk.gov.hmrc.components.compose.ui.theme.HmrcTheme
 
 object SelectRowView {
 
+    private const val RESTRICTED_ITEM_COUNT = 4
+    private const val INITIAL_SELECTED_ROW = -1
+
+    /**
+     * Composable function that creates a SelectRowView with icons and text.
+     *
+     * @param selectRowViewItems List of items to be displayed in the selectable rows.
+     * @param checkedIcon Resource ID for the checked icon.
+     * @param uncheckedIcon Resource ID for the unchecked icon.
+     * @param errorText Error text to be displayed, if any.
+     * @param onRowSelected Callback function triggered when an item is clicked.
+     */
     @Composable
-    operator fun invoke() {
-        // TODO: Will write the select row view
+    operator fun invoke(
+        selectRowViewItems: List<String>,
+        checkedIcon: Int = R.drawable.components_select_row_circle_checked,
+        uncheckedIcon: Int = R.drawable.components_select_row_circle_unchecked,
+        errorText: String = "",
+        onRowSelected: (position: Int, value: String) -> Unit
+    ) {
+        val selectedValue = remember { mutableStateOf(INITIAL_SELECTED_ROW) }
+        val restrictedItems = selectRowViewItems.take(RESTRICTED_ITEM_COUNT)
+
+        Column(Modifier.padding(8.dp)) {
+
+            if (errorText.isNotEmpty()) {
+                Text(
+                    text = errorText,
+                    color = HmrcTheme.colors.hmrcRed
+                )
+            }
+            restrictedItems.forEachIndexed { index, selectRow ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .selectable(
+                            selected = (selectedValue.value == index),
+                            onClick = {
+                                selectedValue.value = index
+                                onRowSelected(index, selectRow)
+                            },
+                            role = Role.RadioButton
+                        )
+                        .padding(HmrcTheme.dimensions.hmrcSpacing8)
+                ) {
+                    IconToggleButton(
+                        checked = selectedValue.value == index,
+                        onCheckedChange = {
+                            selectedValue.value = index
+                            onRowSelected(index, selectRow)
+                        },
+                        modifier = Modifier.size(24.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(
+                                if (selectedValue.value == index) {
+                                    checkedIcon
+                                } else {
+                                    uncheckedIcon
+                                }
+                            ),
+                            contentDescription = null,
+                            tint = HmrcTheme.colors.hmrcBlack
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(HmrcTheme.dimensions.hmrcSpacing16))
+
+                    Text(
+                        text = selectRow,
+                        modifier = Modifier.fillMaxWidth(),
+                        color = HmrcTheme.colors.hmrcBlack
+                    )
+                }
+
+                if (index < selectRowViewItems.size - 1) {
+                    Spacer(modifier = Modifier.height(HmrcTheme.dimensions.hmrcSpacing16))
+                }
+            }
+        }
     }
 }
--- sample-compose-fragments/src/main/java/uk/gov/hmrc/sample_compose_fragments/presentation/screens/molecules/SelectRowViewScreen.kt
+++ sample-compose-fragments/src/main/java/uk/gov/hmrc/sample_compose_fragments/presentation/screens/molecules/SelectRowViewScreen.kt
@@ -1,11 +1,77 @@
 package uk.gov.hmrc.sample_compose_fragments.presentation.screens.molecules
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import uk.gov.hmrc.components.compose.molecule.selectrow.SelectRowView
+import uk.gov.hmrc.components.compose.ui.theme.HmrcTheme
+import uk.gov.hmrc.sample_compose_components.R
+import uk.gov.hmrc.sample_compose_fragments.presentation.screens.sampletemplate.ExamplesSlot
+import uk.gov.hmrc.sample_compose_fragments.presentation.screens.sampletemplate.HmrcCard
+import uk.gov.hmrc.sample_compose_fragments.presentation.screens.sampletemplate.PlaceholderSlot
+import uk.gov.hmrc.sample_compose_fragments.presentation.screens.sampletemplate.ScreenScrollViewColumn
 
 object SelectRowViewScreen {
 
+    private const val THIRD_ROW_VIEW = 3
+
     @Composable
-    operator fun invoke(){
-        // TODO: Write the select row view screen
+    operator fun invoke() {
+        val context = LocalContext.current
+        ScreenScrollViewColumn {
+            PlaceholderSlot {
+                SelectRowView(
+                    selectRowViewItems = listOf("(Body) Description"),
+                    errorText = "This is an error"
+                ) { position, value ->
+                    // Handle the SelectRowView item click listener
+                }
+            }
+
+            ExamplesSlot {
+                //Example one
+                HmrcCard(modifier = Modifier.padding(bottom = HmrcTheme.dimensions.hmrcSpacing16)) {
+                    val errorText = remember { mutableStateOf("") }
+                    SelectRowView(
+                        selectRowViewItems = listOf(
+                            "First row",
+                            "Second row",
+                            "Show an error",
+                            stringResource(id = R.string.longer_text)
+                        ),
+                        errorText = errorText.value
+                    ) { position, value ->
+                        if (position == THIRD_ROW_VIEW - 1) {
+                            errorText.value = "This is an error"
+                        }
+                        Toast.makeText(
+                            context,
+                            "Position: $position selected\nValue: $value",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                //Example two
+                HmrcCard(modifier = Modifier.padding(bottom = HmrcTheme.dimensions.hmrcSpacing16)) {
+                    SelectRowView(
+                        selectRowViewItems = listOf(
+                            "First row",
+                            "Second row",
+                            "Third row",
+                            "Fourth row"
+                        ),
+                        checkedIcon = R.drawable.components_select_row_tick_checked
+                    ) { position, value ->
+                        // We can use this block for Select Row View click listener
+                    }
+                }
+            }
+        }
     }
 }
