6044393ea49ff1fbb149afa5154a6955a2b4697e
Jemma <7998113+jemmaSlater@users.noreply.github.com>
2024-03-12 16:24:06 +0000

    Add info message buttons

--- components-compose/src/main/java/uk/gov/hmrc/components/compose/organism/information/InformationMessageCardView.kt
+++ components-compose/src/main/java/uk/gov/hmrc/components/compose/organism/information/InformationMessageCardView.kt
@@ -15,17 +15,23 @@
  */
 package uk.gov.hmrc.components.compose.organism.information
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -37,34 +43,26 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.dp
 import uk.gov.hmrc.components.compose.R
-import uk.gov.hmrc.components.compose.atom.button.SecondaryButton
+import uk.gov.hmrc.components.compose.atom.button.HmrcButton
 import uk.gov.hmrc.components.compose.organism.HmrcCardView
 import uk.gov.hmrc.components.compose.ui.extensions.enableTalkBackMergeAccessibility
 import uk.gov.hmrc.components.compose.ui.theme.HmrcAlwaysBlack
 import uk.gov.hmrc.components.compose.ui.theme.HmrcBlack
 import uk.gov.hmrc.components.compose.ui.theme.HmrcBlue
 import uk.gov.hmrc.components.compose.ui.theme.HmrcRed
+import uk.gov.hmrc.components.compose.ui.theme.HmrcRippleTheme
 import uk.gov.hmrc.components.compose.ui.theme.HmrcTheme
 import uk.gov.hmrc.components.compose.ui.theme.HmrcWhite
 import uk.gov.hmrc.components.compose.ui.theme.HmrcYellow
 
 
-enum class Type(val headlineBackgroundColor: Color, val headlineTint: Color, val iconColor: Color, val icon: Int) {
+enum class MessageType(val backgroundColor: Color, val headlineTint: Color, val iconColor: Color, val icon: Int) {
     WARNING(HmrcYellow, HmrcAlwaysBlack, HmrcAlwaysBlack, R.drawable.components_ic_warning),
     INFO(HmrcBlue, HmrcWhite, HmrcWhite, R.drawable.ic_info),
     URGENT(HmrcRed, HmrcWhite, HmrcWhite, R.drawable.ic_info),
     NOTICE(HmrcBlack, HmrcWhite, HmrcWhite, R.drawable.components_ic_warning)
-}
-
-/*sealed class InformationMessageButton(val button: SecondaryButton, val isOutlineButton: Boolean = false) {
-    data class ActionButton(val actionButton: SecondaryButton) : InformationMessageButton(actionButton, false)
-    data class OutlineButton(val outlineButton: SecondaryButton) : InformationMessageButton(outlineButton, true)
-}*/
-sealed class InformationMessageButton(val button: SecondaryButton, val isOutlineButton: Boolean = false) {
-    data class ActionButton(val actionButton: SecondaryButton): InformationMessageButton(actionButton, false)
-    data class OutLineButton(val outlineButton: SecondaryButton): InformationMessageButton(outlineButton, true)
-
 }
 
 @Composable
@@ -73,13 +71,13 @@ fun InformationMessageCardView(
     headline: String,
     headlineContentDescription: String = "",
     text: String? = null,
-    type: Type,
-    headlineButtons: List<InformationMessageButton>? = null
+    messageType: MessageType,
+    buttons: List<InformationMessageButton> = emptyList()
 ) {
-    val textColor: Color by remember { mutableStateOf(type.headlineTint) }
-    val iconColor: Color by remember { mutableStateOf(type.iconColor) }
-    val backgroundColor: Color by remember { mutableStateOf(type.headlineBackgroundColor) }
-    val icon: Int by remember { mutableIntStateOf(type.icon) }
+    val textColor: Color by remember { mutableStateOf(messageType.headlineTint) }
+    val iconColor: Color by remember { mutableStateOf(messageType.iconColor) }
+    val backgroundColor: Color by remember { mutableStateOf(messageType.backgroundColor) }
+    val icon: Int by remember { mutableIntStateOf(messageType.icon) }
 
     HmrcCardView(modifier = modifier, customBackgroundColor = backgroundColor) {
         Row(
@@ -87,7 +85,6 @@ fun InformationMessageCardView(
                 .fillMaxWidth()
                 .enableTalkBackMergeAccessibility()
                 .padding(HmrcTheme.dimensions.hmrcSpacing16),
-
             ) {
             Image(
                 modifier = Modifier.size(HmrcTheme.dimensions.hmrcIconSize36),
@@ -119,7 +116,7 @@ fun InformationMessageCardView(
                         end = HmrcTheme.dimensions.hmrcSpacing16,
                         bottom = HmrcTheme.dimensions.hmrcSpacing16
                     )
-                ) {
+            ) {
                 Text(
                     text = text,
                     modifier = Modifier,
@@ -128,18 +125,58 @@ fun InformationMessageCardView(
                 )
             }
         }
-        headlineButtons?.forEach{
-            if (it.isOutlineButton) {
-                it.button.apply {Modifier.
-
-                }
-                    
-            //        strokeWidth = OUTLINE_STROKE_WIDTH
-              //      strokeColor = ContextCompat.getColorStateList(context, type!!.headlineTint)
-                //    setTextColor(ContextCompat.getColorStateList(context, type!!.headlineTint))
-                }
-
+        if (buttons.isNotEmpty()) {
+            buttons.forEach {
+                it.invoke()
+                Spacer(modifier = Modifier.height(HmrcTheme.dimensions.hmrcSpacing8))
             }
-
+            Spacer(modifier = Modifier.height(HmrcTheme.dimensions.hmrcSpacing8))
         }
+    }
+}
+
+class InformationMessageButton(
+    val text: String,
+    val messageType: MessageType,
+    val buttonType: ButtonType = ButtonType.ACTION,
+    val onClick: () -> Unit
+) {
+
+    @Composable
+    operator fun invoke() {
+        val (colors, border) = when (buttonType) {
+            ButtonType.ACTION -> {
+                Pair(
+                    ButtonDefaults.buttonColors(
+                        containerColor = HmrcTheme.colors.hmrcWhite, contentColor = HmrcTheme.colors.hmrcBlue
+                    ),
+                    null
+                )
+            }
+            ButtonType.OUTLINE -> {
+                Pair(
+                    ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent, contentColor = messageType.headlineTint
+                    ),
+                    BorderStroke(width = 1.0.dp, color = messageType.headlineTint,)
+                )
+            }
+        }
+        CompositionLocalProvider(LocalRippleTheme provides HmrcRippleTheme) {
+            HmrcButton(
+                text = text,
+                modifier = Modifier.padding(horizontal = HmrcTheme.dimensions.hmrcSpacing16),
+                contentPadding = PaddingValues(all = HmrcTheme.dimensions.hmrcSpacing16),
+                buttonColors = colors,
+                border = border,
+                onClick = onClick,
+            )
+        }
+    }
+
+    companion object {
+        enum class ButtonType {
+            ACTION, OUTLINE
+        }
+    }
 }
