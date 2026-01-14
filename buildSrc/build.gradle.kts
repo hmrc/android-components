321046c81178b93fae5d81d4c38d68208e53c033
gayathria-hmrc <248484011+gayathria-hmrc@users.noreply.github.com>
2026-01-14 21:39:59 +0000

    HMA-11141 Specified Java Compile version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 repositories {
     mavenCentral()
 }
 
 plugins {
     `kotlin-dsl`
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "17"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "17"
 }
