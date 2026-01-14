186440b083768425449bb0d13a9a7ba7773ef679
gayathria-hmrc <248484011+gayathria-hmrc@users.noreply.github.com>
2026-01-14 15:21:12 +0000

    HMA-11141 Added Copyright

--- components-compose/src/main/java/uk/gov/hmrc/components/compose/organism/menu/MenuPanelRowView.kt
+++ components-compose/src/main/java/uk/gov/hmrc/components/compose/organism/menu/MenuPanelRowView.kt
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gov.hmrc.components.compose.organism.menu
 
-import android.app.Notification
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,7 +33,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import uk.gov.hmrc.components.compose.R
