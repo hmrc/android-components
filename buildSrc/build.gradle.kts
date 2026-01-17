6dbd80263fcdfb9991bd199b03edeaad4c98d62b
gayathria-hmrc <248484011+gayathria-hmrc@users.noreply.github.com>
2026-01-17 22:19:00 +0000

    HMA-11141 Added javaCompile to use Java 17

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,16 @@ plugins {
     `kotlin-dsl`
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "17"
