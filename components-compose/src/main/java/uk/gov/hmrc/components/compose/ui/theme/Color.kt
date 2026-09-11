e5e270747528921fcd3133ff36dd2c8e36389b00
Harry-Accent <271540689+Harry-Accent@users.noreply.github.com>
2026-09-11 13:56:48 +0100

    HMA-12503 added new colours to the xml and colors.kt file

--- components-compose/src/main/java/uk/gov/hmrc/components/compose/ui/theme/Color.kt
+++ components-compose/src/main/java/uk/gov/hmrc/components/compose/ui/theme/Color.kt
@@ -19,16 +19,12 @@ import androidx.compose.ui.graphics.Color
 
 // Light theme colors
 val HmrcBlack = Color(0xFF0B0C0C)
-val HmrcWhite = Color(0xFFFFFFFF)
-val HmrcWhiteBackground = Color(0xFFFFFFFF)
-val HmrcGreen1 = Color(0xFF00703C)
 val HmrcGreen2 = Color(0xFF85994B)
-val HmrcBlue = Color(0xFF1D70B8)
 val HmrcTeal = Color(0xFF28A197)
 val HmrcRed = Color(0xFFD4351C)
+//
 val HmrcGrey1 = Color(0xFF505A5F)
 val HmrcGrey2 = Color(0xFFB1B4B6)
-val HmrcGrey3 = Color(0xFFF3F2F1)
 val HmrcPink = Color(0xFFCA2B75)
 val HmrcYellow = Color(0xFFFFBF47)
 val HmrcAlwaysBlack = Color(0xFF0B0C0C)
@@ -44,12 +40,29 @@ val HmrcGrey2Dark = Color(0xFF6F777B)
 val HmrcPinkDark = Color(0xFFBB94FF)
 val HmrcYellowDark = Color(0xFFFEFF4F)
 
+//light mode colours that pair with the changing dark mode colors
+val HmrcWhite = Color(0xFFFFFFFF)
+val HmrcWhiteBackground = Color(0xFFFFFFFF)
+val HmrcBlue = Color(0xFF1D70B8)
+val HmrcGreen1 = Color(0xFF00703C)
+val HmrcGrey3 = Color(0xFFF3F2F1)
+
 //old dark mode colors (to be removed once all changes are made)
 val HmrcWhiteDark = Color(0xFF0B0C0C)
 val HmrcWhiteBackgroundDark = Color(0xFF262626)
 val HmrcBlueDark = Color(0xFF5BC0C6)
 val HmrcGreen1Dark = Color(0xFF69B134)
 val HmrcGrey3Dark = Color(0xFF0B0C0C)
+
+//new light mode colors (name changes)
+
+val HmrcPrimaryGrey = Color(0xFF282D30)
+val HmrcDarkGrey = Color(0xFF505A5F)
+val HmrcMidGrey = Color(0xFFB1B4B6)
+val HmrcLightGrey = Color(0xFFF3F2F1)
+val HmrcPrimaryTeal = Color(0xFF5BC0C6)
+val HmrcPrimaryYellow = Color(0xFFFFDD00)
+val HmrcPrimaryLightBlue = Color(0xFFD7E4F2)
 
 
 //new dark mode colors
@@ -59,3 +72,4 @@ val HmrcDarkNavy = Color(0xFF0D1C29)
 val HmrcDarkNavy2 = Color(0xFF061625)
 val HmrcDarkNavy3 = Color(0xFF092537)
 val HmrcNavy = Color(0xFF0A2740)
+//mid-navy still to be added to excel spreadsheet
