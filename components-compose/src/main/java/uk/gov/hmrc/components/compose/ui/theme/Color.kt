2aae6b7f79a3509132c46033531ce07c05a512b7
Harry-Accent <271540689+Harry-Accent@users.noreply.github.com>
2026-09-15 15:58:38 +0100

    HMA-12503 added new SemanticColors file with the semantic tokens set up, added in XML as well

--- components-compose/src/main/java/uk/gov/hmrc/components/compose/ui/theme/Color.kt
+++ components-compose/src/main/java/uk/gov/hmrc/components/compose/ui/theme/Color.kt
@@ -40,21 +40,21 @@ val HmrcGrey2Dark = Color(0xFF6F777B)
 val HmrcPinkDark = Color(0xFFBB94FF)
 val HmrcYellowDark = Color(0xFFFEFF4F)
 
-//light mode colours that pair with the changing dark mode colors
+// light mode colours that pair with the changing dark mode colors
 val HmrcWhite = Color(0xFFFFFFFF)
 val HmrcWhiteBackground = Color(0xFFFFFFFF)
 val HmrcBlue = Color(0xFF1D70B8)
 val HmrcGreen1 = Color(0xFF00703C)
 val HmrcGrey3 = Color(0xFFF3F2F1)
 
-//old dark mode colors (to be removed once all changes are made)
+// old dark mode colors (to be removed once all changes are made)
 val HmrcWhiteDark = Color(0xFF0B0C0C)
 val HmrcWhiteBackgroundDark = Color(0xFF262626)
 val HmrcBlueDark = Color(0xFF5BC0C6)
 val HmrcGreen1Dark = Color(0xFF69B134)
 val HmrcGrey3Dark = Color(0xFF0B0C0C)
 
-//new light mode colors (name changes)
+// new light mode colors (name changes)
 
 val HmrcPrimaryGrey = Color(0xFF282D30)
 val HmrcDarkGrey = Color(0xFF505A5F)
@@ -64,12 +64,11 @@ val HmrcPrimaryTeal = Color(0xFF5BC0C6)
 val HmrcPrimaryYellow = Color(0xFFFFDD00)
 val HmrcPrimaryLightBlue = Color(0xFFD7E4F2)
 
-
-//new dark mode colors
+// new dark mode colors
 val HmrcPrimaryGreen = Color(0xFF188659)
 val HmrcPrimaryRed = Color(0xFFF26954)
 val HmrcDarkNavy = Color(0xFF0D1C29)
 val HmrcDarkNavy2 = Color(0xFF061625)
 val HmrcDarkNavy3 = Color(0xFF092537)
 val HmrcNavy = Color(0xFF0A2740)
-//mid-navy still to be added to excel spreadsheet
+// mid-navy still to be added to excel spreadsheet
